6181e435aa1a236a0092537b15d2a199da38d3fa
Kathan Cheema <kathancheema@outlook.com>
2020-06-20 00:30:47 +0100

    Fix CI issues.
    - Change CSS as there is an issue in react-scripts build with recursive calc/var references.
    - Fix lombok issues by using annotation processor.

--- src/main/js/src/App.test.tsx
+++ src/main/js/src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 afterEach(cleanup);
 
 test('given a new app, when the app loads, it asks the user to start or join a game', async () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App/>);
   const element = await waitForElement(() => getByText(/Start new game/i));
   expect(element).toBeInTheDocument();
 });
